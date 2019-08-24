0e4fe472b7cab57e33f6dc9bdfd14a286dd197a3
Mike Henderyckx <mike.henderyckx@gmail.com>
2019-08-24 18:44:13 +0200

    Replace sanitize.css with custom src/reset.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import 'sanitize.css';
+import './reset.css';
 import App from './App.js';
 
 ReactDOM.render(
