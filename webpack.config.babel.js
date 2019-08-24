3ff0d47528da3ce9b57e5eace6ab50cc97efb66f
Mike Henderyckx <mike.henderyckx@gmail.com>
2019-08-24 15:33:14 +0200

    Conditionally use plugins for 'development' & 'production' on webpack.config. Also add sourcemaps for better debugging experience

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,37 +3,13 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export default (env, argv) => ({
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: argv.mode === 'development' ? '/' : './',
-    filename: '[name].js',
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
+function getPlugins(mode) {
+  let plugins = [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       hash: true,
       title: 'My App', // todo: read from config
-      favicon: 'src/images/favicon.ico',
+      favicon: './src/images/favicon.ico',
       openGraph: {
         url: 'http://my-app.com',
         type: 'article',
@@ -46,9 +22,53 @@ export default (env, argv) => ({
       analyzerMode: 'static',
       openAnalyzer: false,
     }),
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
-});
+  ];
+
+  if (mode === 'development') {
+    plugins = plugins.concat([new webpack.HotModuleReplacementPlugin()]);
+  }
+
+  if (mode === 'production') {
+    plugins = plugins.concat([new CleanWebpackPlugin()]);
+  }
+
+  return plugins;
+}
+
+export default (env, argv) => {
+  return {
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: './',
+      filename: '[name].js',
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: getPlugins(argv.mode),
+    devtool: argv.mode === 'development' ? 'inline-source-map' : 'source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true,
+    },
+  };
+};
