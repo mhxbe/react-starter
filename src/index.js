7b42d1d70401d453f0765dad91d9bfb7ac73acce
Mike Henderyckx <mike.henderyckx@gmail.com>
2019-08-24 21:35:56 +0200

    Remove react-router-dom dependency on src/index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import './reset.css';
 import App from './App.js';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App name="Mikeeeh" />
-  </BrowserRouter>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App name="Mikeeeh" />, document.querySelector('#root'));
 
 if (module.hot) {
   module.hot.accept();
