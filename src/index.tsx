cf571093acff885a264a383a61ed5b11e9326df5
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-03-20 23:48:22 +0100

    Remove import reset.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import './reset.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
