a9ba3413c1973b62147cbce9a9ce2248bf03bc76
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-03-21 01:50:13 +0100

    Add WebFront & load Open Sans from Google Fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as WebFont from 'webfontloader';
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
@@ -7,3 +8,9 @@ ReactDOM.render(<App />, document.querySelector('#root'));
 if (module.hot) {
   module.hot.accept();
 }
+
+WebFont.load({
+  google: {
+    families: ['Open+Sans:400,700,800&display=swap'],
+  },
+});
