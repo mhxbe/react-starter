bef64e64554abc279df91c4206a23374963e00bb
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-03-26 20:15:05 +0100

    Add workbox-window in index.tsx to register serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as WebFont from 'webfontloader';
+import { Workbox } from 'workbox-window';
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
 if (module.hot) {
   module.hot.accept();
+}
+
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  const wb = new Workbox('./sw.js');
+
+  wb.addEventListener('installed', (event) => {
+    if (event.isUpdate) {
+      if (confirm('Update available. Click ok to renew app.')) {
+        window.location.reload();
+      }
+    } else {
+      console.log('App successfully installed!');
+    }
+  });
+
+  wb.addEventListener('activated', (event) => {
+    if (!event.isUpdate) {
+      console.log('Service worker activated for the first time!');
+    }
+  });
+
+  wb.register();
 }
 
 WebFont.load({
