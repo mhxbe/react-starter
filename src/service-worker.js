cab78035e371719d3bebe03a1181b577f5e47e53
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-03-26 20:14:10 +0100

    Add src/serviceWorker.js

---
+++ src/service-worker.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
+);
+
+workbox.setConfig({
+  debug: false,
+});
+
+workbox.core.setCacheNameDetails({
+  prefix: 'my-app',
+  suffix: '0.0.1',
+});
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.routing.registerRoute(
+  new RegExp(/\.(?:png|gif|jpg|jpeg|svg)$/),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images-cache',
+  })
+);
