5a064640757993cce34d5c77ab2dffe02670622c
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-03-27 23:15:37 +0100

    Wrap service-worker.ts contents with if (workbox) so it doesn't crash when workbox can't be loaded

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,40 +6,42 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
 );
 
-workbox.setConfig({
-  debug: false,
-});
+if (workbox) {
+  workbox.setConfig({
+    debug: false,
+  });
 
-workbox.core.setCacheNameDetails({
-  prefix: name,
-  suffix: version,
-}); // results in: {prefix}-precache-v2-{suffix} -> e.g.: react-starter-precache-v2-1.0.0
+  workbox.core.setCacheNameDetails({
+    prefix: name,
+    suffix: version,
+  }); // results in: {prefix}-precache-v2-{suffix} -> e.g.: react-starter-precache-v2-1.0.0
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
 
-// Cache images with a cache first strategy.
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'images-cache',
-  })
-);
+  // Cache images with a cache first strategy.
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images-cache',
+    })
+  );
 
-// Cache the Google Fonts stylesheets with a stale while revalidate strategy.
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'google-fonts-stylesheets',
-  })
-);
+  // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+  );
 
-// Cache the Google Fonts webfont files with a cache first strategy.
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'google-fonts-webfonts',
-  })
-);
+  // Cache the Google Fonts webfont files with a cache first strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'google-fonts-webfonts',
+    })
+  );
+}
