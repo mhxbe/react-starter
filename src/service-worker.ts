8584d5ffe210cef1a603a41ccd694414dbf176fc
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-05-24 03:34:04 +0200

    Add @ts-ignore

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,6 +3,8 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: __WB_MANIFEST does not exist on window
 precacheAndRoute(self.__WB_MANIFEST);
 
 skipWaiting();
