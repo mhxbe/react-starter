5eccbf91ce163047e67f8de6573f9ea5a27aee84
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-09-12 20:06:01 +0200

    Refactor service-worker.ts workbox code to v6 + add support for routing (react-router-dom)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,13 +1,15 @@
-import { skipWaiting, clientsClaim } from 'workbox-core';
-import { precacheAndRoute } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
+import { clientsClaim } from 'workbox-core';
+import { createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 import { CacheFirst } from 'workbox-strategies';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: __WB_MANIFEST does not exist on window
 precacheAndRoute(self.__WB_MANIFEST);
 
-skipWaiting();
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+self.skipWaiting();
 clientsClaim();
 
 // Cache images with a cache first strategy.
@@ -17,3 +19,8 @@ registerRoute(
     cacheName: 'images-cache',
   })
 );
+
+// Reroute navigation requests to index.html
+const handler = createHandlerBoundToURL('/index.html');
+const navigationRoute = new NavigationRoute(handler);
+registerRoute(navigationRoute);
