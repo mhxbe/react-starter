0b3339f452b8aff757470153804de1b9d65536f2
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-09-12 13:40:26 +0200

    Code split different routes (Suspense)

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,16 @@ import useShowSidebar from './hooks/show-sidebar';
 import Header from './common/Header';
 import Sidebar from './common/Sidebar';
 import ErrorFallback, { errorHandler } from './common/ErrorFallback';
-import Home from './pages/Home';
-import About from './pages/About';
-import PageNotFound from './pages/404';
+
+const Home = React.lazy(
+  () => import(/* webpackChunkName: 'home' */ './pages/Home')
+);
+const About = React.lazy(
+  () => import(/* webpackChunkName: 'about' */ './pages/About')
+);
+const PageNotFound = React.lazy(
+  () => import(/* webpackChunkName: '404' */ './pages/404')
+);
 
 const App: React.FC = () => {
   const {
@@ -37,40 +44,42 @@ const App: React.FC = () => {
           toggleSidebarRef={toggleSidebarRef}
         />
         <Content aria-hidden={!showContent}>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <ErrorBoundary
-                  key="home"
-                  FallbackComponent={ErrorFallback}
-                  onError={errorHandler}
-                >
-                  <Home />
-                </ErrorBoundary>
-              )}
-            />
-            <Route
-              path="/about"
-              render={() => (
-                <ErrorBoundary
-                  key="about"
-                  FallbackComponent={ErrorFallback}
-                  onError={errorHandler}
-                >
-                  <About />
-                </ErrorBoundary>
-              )}
-            />
-            <Route
-              path="/404"
-              component={(props: RouteComponentProps) => (
-                <PageNotFound {...props} />
-              )}
-            />
-            <Redirect to="/404" />
-          </Switch>
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <ErrorBoundary
+                    key="home"
+                    FallbackComponent={ErrorFallback}
+                    onError={errorHandler}
+                  >
+                    <Home />
+                  </ErrorBoundary>
+                )}
+              />
+              <Route
+                path="/about"
+                render={() => (
+                  <ErrorBoundary
+                    key="about"
+                    FallbackComponent={ErrorFallback}
+                    onError={errorHandler}
+                  >
+                    <About />
+                  </ErrorBoundary>
+                )}
+              />
+              <Route
+                path="/404"
+                component={(props: RouteComponentProps) => (
+                  <PageNotFound {...props} />
+                )}
+              />
+              <Redirect to="/404" />
+            </Switch>
+          </React.Suspense>
         </Content>
       </Main>
     </>
