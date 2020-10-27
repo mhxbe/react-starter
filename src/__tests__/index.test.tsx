641b1ed4ceb264dfd6132be8c8f1e5484854ee37
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-10-27 23:45:21 +0100

    Write tests for ServiceWorker (Workbox) registration & eventListeners

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -19,3 +19,70 @@ describe('Application root', () => {
     );
   });
 });
+
+describe('Workbox ServiceWorker', () => {
+  const OLD_ENV = process.env;
+  const events: any = {};
+  const mockWindowLocationReload = jest.fn();
+  Object.defineProperty(global.navigator, 'serviceWorker', { value: {} });
+  Object.defineProperty(window, 'location', {
+    value: {
+      reload: mockWindowLocationReload,
+    },
+  });
+  const mockServiceWorkerRegister = jest.fn();
+  const mockWorkBox = jest.fn().mockReturnValue({
+    register: mockServiceWorkerRegister,
+    addEventListener: jest.fn((event, callback) => {
+      events[event] = callback;
+    }),
+  });
+  jest.mock('workbox-window', () => ({
+    Workbox: mockWorkBox,
+  }));
+
+  beforeEach(() => {
+    jest.resetModules(); // clear cache
+    process.env = { ...OLD_ENV };
+  });
+
+  it('should not register the serviceWorker when NODE_ENV is not "production"', () => {
+    process.env.NODE_ENV = 'development';
+    require('../index.tsx');
+    expect(mockServiceWorkerRegister).not.toHaveBeenCalled();
+  });
+
+  it('should register the serviceWorker when NODE_ENV is "production"', () => {
+    process.env.NODE_ENV = 'production';
+    require('../index.tsx');
+    expect(mockServiceWorkerRegister).toHaveBeenCalled();
+  });
+
+  it("should handle the 'installed' event", () => {
+    process.env.NODE_ENV = 'production';
+    require('../index.tsx');
+    const consoleSpy = jest.spyOn(console, 'log');
+    events.installed({ isUpdate: false });
+    expect(consoleSpy).toHaveBeenCalledWith('App successfully installed!');
+
+    events.installed({ isUpdate: true });
+    expect(mockWindowLocationReload).toHaveBeenCalled();
+  });
+
+  it("should handle the 'activated' event", () => {
+    process.env.NODE_ENV = 'production';
+    require('../index.tsx');
+    const consoleSpy = jest.spyOn(console, 'log');
+    events.activated({ isUpdate: false });
+    expect(consoleSpy).toHaveBeenCalledWith(
+      'Service worker activated for the first time!'
+    );
+
+    events.activated({ isUpdate: true });
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+    jest.resetAllMocks();
+  });
+});
