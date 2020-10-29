bc1eb5dbe80e5e1e82972d4bfbcfc96764b1cab7
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-10-29 22:15:42 +0100

    Add i18next in jest.setup.ts & fix borken test on 404.test.tsx

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,2 @@
 import '@testing-library/jest-dom';
+import './src/i18n';
--- src/pages/__tests__/404.test.tsx
+++ src/pages/__tests__/404.test.tsx
@@ -21,5 +21,5 @@ test('Displays a title', () => {
       <WrappedComponent />
     </BrowserRouter>
   );
-  expect(screen.getByText('Page Not Found')).toBeInTheDocument();
+  expect(screen.getByText('Page not found')).toBeInTheDocument();
 });
