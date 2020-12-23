6bb6ecf43aa4bdb8dd26d09dd121534522594a40
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-12-24 00:10:20 +0100

    Add utils/test-util.tsx to setupFilesAfterEnv

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config.InitialOptions = {
   testResultsProcessor: 'jest-sonar-reporter',
   collectCoverage: false,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['./jest.setup.ts', './utils/test-util.tsx'],
   moduleNameMapper: {
     'react-i18next': '<rootDir>/__mocks__/react-i18next.tsx',
     i18next: '<rootDir>/__mocks__/i18next.ts',
