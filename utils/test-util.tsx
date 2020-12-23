b221e8145fe9f67038453f092e65623f118af0ea
Mike Henderyckx <mike.henderyckx@gmail.com>
2020-12-24 00:11:05 +0100

    Fix Type Error on utils/test-util

--- utils/test-util.tsx
+++ utils/test-util.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import * as React from 'react';
 import { render, RenderResult, RenderOptions } from '@testing-library/react';
 import { ThemeProvider } from '@emotion/react';
 import { lightTheme } from '../src/themes';
 
-const AllTheProviders: React.FC = ({ children }) => {
-  return <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>;
+const AllTheProviders: React.FC = (props) => {
+  return <ThemeProvider theme={lightTheme}>{props.children}</ThemeProvider>;
 };
 
 const customRender = (
   ui: React.ReactElement,
-  options: RenderOptions
+  options?: RenderOptions
 ): RenderResult => {
   return render(ui, { wrapper: AllTheProviders, ...options });
 };
