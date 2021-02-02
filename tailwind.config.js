490a576c32f8aff14004d8d2d46e3ce0208b8da8
Mike Henderyckx <mike.henderyckx@gmail.com>
2021-02-02 23:34:05 +0100

    Add postcss-focus-visible & some custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,38 @@ module.exports = {
   purge: ['./src/**/*.tsx'],
   darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      normal: '#282a37',
+      black: '#000000',
+      white: '#ffffff',
+      cyan: '#00d8ff',
+      lightCyan: '#00d9fe',
+      teal: '#015b6d',
+      veryLightCyan: 'lightcyan',
+      navy: '#060060',
+      lightGray: '#d9d9d9',
+      darkGray: '#333333',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      lineHeight: {
+        12: '3rem',
+      },
+      width: {
+        300: '300px',
+      },
+    },
   },
-  variants: {},
+  variants: {
+    extend: {
+      backgroundColor: ['focus-visible'],
+      borderWidth: ['first'],
+      margin: ['last'],
+      textColor: ['visited', 'focus-visible'],
+    },
+  },
   plugins: [],
 };
