62b2d0f6bebcb643f764b10ba934a20211413045
Mike Henderyckx <mike.henderyckx@gmail.com>
2021-03-04 20:57:23 +0100

    Add dynamic data-testid to SkipLink component

--- src/common/SkipLink.tsx
+++ src/common/SkipLink.tsx
@@ -8,6 +8,7 @@ interface SkipLinkProps {
 const Header: React.FC<SkipLinkProps> = ({ href, text }) => {
   return (
     <a
+      data-testid={`skip-link-${href}`}
       href={href}
       className="custom-skip-link overflow-hidden absolute whitespace-nowrap focus:items-center focus:bg-darkGray focus:text-white focus:flex focus:overflow-auto focus:no-underline focus:py-0 focus:px-2 focus:leading-10 focus:border-2 focus:border-white focus:z-40"
     >
