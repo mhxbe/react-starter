182ce942061f94791659713eba6b5ec1319c587a
Mike Henderyckx <mike.henderyckx@gmail.com>
2021-11-07 13:05:46 +0100

    fix: remove Switch & Redirect & refactor Route components to new v6 api

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -6,17 +6,15 @@ import {
   fireEvent,
   act,
 } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
 import App from '../App';
 import * as useWindowWidth from '../hooks/useWindowWidth';
 import { BREAKPOINT_DESKTOP } from '../constants';
+import { BrowserRouter } from 'react-router-dom';
 
-const history = createMemoryHistory();
 const renderAppComponent = () => (
-  <Router history={history}>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
 
 test('Should render a header, main & sidebar elements', () => {
@@ -25,56 +23,6 @@ test('Should render a header, main & sidebar elements', () => {
   expect(screen.getByTestId('header')).toBeInTheDocument();
   expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   expect(screen.getByRole('main')).toBeInTheDocument();
-});
-
-describe('Rendering components through routing', () => {
-  test('Render the Home component by default', () => {
-    render(renderAppComponent());
-    expect(screen.getByTestId('page-home')).toBeInTheDocument();
-  });
-
-  test('Navigate to /about renders a Loading component and afterwards the About component when loading is succesful', async () => {
-    history.push('/en/about');
-    render(renderAppComponent());
-
-    expect(screen.getByTestId('page-loading')).toBeInTheDocument();
-    await waitFor(() =>
-      expect(screen.getByTestId('page-about')).toBeInTheDocument()
-    );
-  });
-
-  test('Navigating back to /home after should immediately show Home component without loading', () => {
-    history.push('/en/about');
-    history.push('/');
-    render(renderAppComponent());
-
-    expect(screen.getByTestId('page-home')).toBeInTheDocument();
-  });
-
-  test('Redirect to 404(PageNotFound) when navigating to unexisting route', async () => {
-    history.push('/some/bad/route');
-    render(renderAppComponent());
-
-    expect(screen.getByTestId('page-loading')).toBeInTheDocument();
-    await waitFor(() =>
-      expect(screen.getByTestId('page-404')).toBeInTheDocument()
-    );
-  });
-
-  test('Sidebar is hidden by default & toggleble by clicking MenuIconToggle', () => {
-    render(renderAppComponent());
-
-    expect(screen.getByTestId('sidebar')).toHaveAttribute(
-      'aria-hidden',
-      'true'
-    );
-
-    fireEvent.click(screen.getByTestId('toggle-sidebar'));
-    expect(screen.getByTestId('sidebar')).toHaveAttribute(
-      'aria-hidden',
-      'false'
-    );
-  });
 });
 
 test('toggling of the sidebar, settings correct aria-labels on Sidebar & Content', async () => {
