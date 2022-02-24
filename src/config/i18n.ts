f0bb6e1837479040be91fce838bfd4f5119de3b8
Mike Henderyckx <mike.henderyckx@gmail.com>
2022-02-24 20:18:16 +0100

    fix: use named export of use instead of i18n.use

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,10 +1,9 @@
-import i18n from 'i18next';
+import { use } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export default i18n
-  .use(Backend)
+export default use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
